d17f3b521ada95472585e783d19997b49492860d
Yuzhi.yz <shenfushan.sfs@alibaba-inc.com>
2022-08-25 17:41:49 +0800

    jdk17

--- mse-simple-demo/A/build.sh
+++ mse-simple-demo/A/build.sh
@@ -3,5 +3,9 @@ set -e
 
 cd "$(dirname "$0")"
 
+export JAVA_17_HOME=`/usr/libexec/java_home -v 17`
+export JAVA_HOME=$JAVA_17_HOME
+
 mvn clean package
-docker build . -t spring-cloud-a:1.0.0
+docker build . -t registry.cn-hangzhou.aliyuncs.com/yuzhi-1998/spring-cloud-a:1.0.0 --platform linux/amd64
+docker push registry.cn-hangzhou.aliyuncs.com/yuzhi-1998/spring-cloud-a:1.0.0
